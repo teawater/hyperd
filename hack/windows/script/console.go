963b537a358ef34ea44ef6b340f47c4bee7380aa
Jimmy Xu <xjimmyshcn@gmail.com>
2017-04-27 07:11:44 +0100

    [windows-support] optimize console(use github.com/chzyer/readline)

--- hack/windows/script/console.go
+++ hack/windows/script/console.go
@@ -8,7 +8,7 @@ import (
 	"os"
 	"strings"
 
-	"github.com/carmark/pseudo-terminal-go/terminal"
+	"github.com/chzyer/readline"
 )
 
 func reader(r io.Reader, c chan int) {
@@ -25,51 +25,62 @@ func reader(r io.Reader, c chan int) {
 		if strings.HasSuffix(buf.String(), "\n\nDONE\n\n") {
 			fmt.Printf("%v", strings.Replace(buf.String(), "\n\nDONE\n\n", "", -1))
 			c <- 0
+			buf.Reset()
 		}
 	}
 }
 
-func interactive_exec(ctl net.Conn, c chan int, hostname *string, shell *string) error {
+func interactive_exec(ctl net.Conn, c chan int, hostname string, shell string) error {
 	//fmt.Printf("[debug] interactive_exec\n")
 
-	term, err := terminal.NewWithStdInOut()
+	l, err := readline.NewEx(&readline.Config{
+		Prompt:            fmt.Sprintf("%v C:\\Windows\\System32>", hostname),
+		HistoryFile:       "/tmp/demo-history.tmp",
+		InterruptPrompt:   "^C",
+		EOFPrompt:         "exit",
+		HistorySearchFold: true,
+	})
 	if err != nil {
 		panic(err)
 	}
-	defer term.ReleaseFromStdInOut() // defer this
-	fmt.Println("Ctrl-D/exit/quit to break")
-	term.SetPrompt(fmt.Sprintf("%s C:\\Windows\\system32>", *hostname))
-	line, err := term.ReadLine()
+	defer l.Close()
+
 	for {
-		if err == io.EOF {
-			term.Write([]byte(line))
-			fmt.Println()
-			return nil
-		}
-		if (err != nil && strings.Contains(err.Error(), "control-c break")) || len(line) == 0 {
-			line, err = term.ReadLine()
-		} else if line == "exit" || line == "quit" {
-			break
-		} else {
-			//term.Write([]byte(line+"\r\n"))
-			cmd := ""
-			switch *shell {
-			case "powershell":
-				cmd = fmt.Sprintf("%s %s\r\n", *shell, line)
-			case "cmd":
-				cmd = fmt.Sprintf("%s /c %s\r\n", *shell, line)
+		line, err := l.Readline()
+		if err == readline.ErrInterrupt {
+			if len(line) == 0 {
+				break
+			} else {
+				continue
 			}
-			noninteractive_exec(ctl, c, &cmd)
-			line, err = term.ReadLine()
+		} else if err == io.EOF {
+			break
+		}
+
+		line = strings.TrimSpace(line)
+		switch {
+		case line == "quit" || line == "exit":
+			goto exit
+		case line == "":
+			continue
+		default:
+			cmd := ""
+			switch shell {
+			case "powershell":
+				cmd = fmt.Sprintf("%s %s\n", shell, line)
+			case "cmd":
+				cmd = fmt.Sprintf("%s /c %s\n", shell, line)
+			}
+			noninteractive_exec(ctl, c, cmd)
 		}
 	}
-
+exit:
 	return nil
 }
 
-func noninteractive_exec(ctl net.Conn, c chan int, cmd *string) error {
-	//fmt.Printf("[debug] noninteractive_exec\n")
-	_, err := ctl.Write([]byte(*cmd))
+func noninteractive_exec(ctl net.Conn, c chan int, cmd string) error {
+	//fmt.Printf("[debug] noninteractive_exec - cmd: [%v]\n", cmd)
+	_, err := ctl.Write([]byte(cmd))
 	if err != nil {
 		fmt.Printf("write error:", err)
 		return err
@@ -120,8 +131,8 @@ func main() {
 	go reader(tty, c)
 
 	if interactive {
-		interactive_exec(ctl, c, &hostname, &shell)
+		interactive_exec(ctl, c, hostname, shell)
 	} else {
-		noninteractive_exec(ctl, c, &cmd)
+		noninteractive_exec(ctl, c, cmd)
 	}
 }
